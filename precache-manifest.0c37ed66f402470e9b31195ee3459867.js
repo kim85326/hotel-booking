c863a8f55c19f01ab5f21ab66d58e31452e14782
elaine <kim85326@gmail.com>
2019-08-25 18:06:23 +0800

    Updates

---
+++ precache-manifest.0c37ed66f402470e9b31195ee3459867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8653b1964b1f54cf6180f55f0e1a7d70",
+    "url": "/hotel-booking/index.html"
+  },
+  {
+    "revision": "75348797423383d7fb83",
+    "url": "/hotel-booking/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "40e5e374c354c9fb8936",
+    "url": "/hotel-booking/static/css/main.f471575a.chunk.css"
+  },
+  {
+    "revision": "75348797423383d7fb83",
+    "url": "/hotel-booking/static/js/2.704e7975.chunk.js"
+  },
+  {
+    "revision": "40e5e374c354c9fb8936",
+    "url": "/hotel-booking/static/js/main.759ee9fb.chunk.js"
+  },
+  {
+    "revision": "99d62e5765110ed6ba17",
+    "url": "/hotel-booking/static/js/runtime~main.2346735c.js"
+  }
+]);
